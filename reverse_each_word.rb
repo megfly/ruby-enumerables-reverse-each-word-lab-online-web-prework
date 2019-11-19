6456166d0403b18aca148a74ba2dd933c9d64ff2
Meghan Flynn <mflynn333@gmail.com>
2019-11-19 22:59:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,6 @@
 def reverse_each_word(string)
   array = string.split 
   array.each do |words|
-    words.reverse! 
-  end 
-  array.join (" ")
-end 
-
-
-def reverse_each_word(string)
-  array = string.split 
-  array.collect do |words|
     words.reverse 
   end 
   array.join (" ")
